8f153d7df50899ba96259d9594f768f82e31be13
AMAL RAJ T <amal.art.art@gmail.com>
2023-09-28 21:39:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
-import {BiSearch} from "react-icons/bi"
-import {BiMicrophone} from "react-icons/bi"
+import { BiSearch } from "react-icons/bi";
+import { BiMicrophone } from "react-icons/bi";
 
 function App() {
   return (
@@ -11,9 +11,13 @@ function App() {
       <div className="google">
         <h1 className="title">Google</h1>
         <div className="input">
-        <BiSearch size={30} color="grey"/>
-          <input type="text" placeholder="Search Google or type a URL" className="search" />
-          <BiMicrophone size={20} color="blue"/>
+          <BiSearch size={20} color="grey" />
+          <input
+            type="text"
+            placeholder="Search Google or type a URL"
+            className="search"
+          />
+          <BiMicrophone size={20} color="blue" />
         </div>
       </div>
     </div>
